a2453a3be64adeb399adb12219165e4464246c22
Anthony Ettinger <anthony@chovy.com>
2020-02-20 10:02:44 -0800

    add body

--- controllers/retrieve.js
+++ controllers/retrieve.js
@@ -2,7 +2,9 @@ import fetch from 'node-fetch';
 import cheerio from 'cheerio';
 
 export const get = async (req, res) => {
-  const doc = await fetch(req.query.url)
+  const url = decodeURIComponent(req.query.url);
+  console.log('retrieve', url);
+  const doc = await fetch(url)
     .then(doc => doc.text())
     .catch(console.error);
 
@@ -11,6 +13,7 @@ export const get = async (req, res) => {
 
   res.json({
     title,
+    doc,
   });
 };
 
